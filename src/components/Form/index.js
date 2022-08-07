66755e03354cffb2c620e23340c56c5e08f65e08
sevvalsaygat <sevvallsstt@gmail.com>
2022-08-08 00:41:49 +0300

    Delete button and add new item button edit

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -142,18 +142,22 @@ function Index({ invoices, setInvoices }) {
                     }
                   </td>
                   <td>
-                    <button onClick={() => {
-                      remove(i)
-                    }}>Delete</button>
+                    <button className="mt-5 px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+                      type='submit'
+                      onClick={() => {
+                        remove(i)
+                      }}>Delete</button>
                   </td>
                 </tr>
               ))
             }
           </tbody>
         </table>
-        <button onClick={() => {
-          append(defaultItemValue)
-        }}>Add New Item</button>
+        <button className="mt-5 mr-5 px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"
+          type='submit'
+          onClick={() => {
+            append(defaultItemValue)
+          }}>Add New Item</button>
 
         <button
           className="mt-5 px-4 py-2 font-medium tracking-wide text-white capitalize transition-colors duration-200 transform bg-blue-600 rounded-md hover:bg-blue-500 focus:outline-none focus:ring focus:ring-blue-300 focus:ring-opacity-80"

bc4ac50d43cd526cead52e74b73f394b50ef67ca
sevvalsaygat <sevvallsstt@gmail.com>
2022-08-11 19:55:46 +0300

    Use watch hook on create form

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -9,7 +9,7 @@ const defaultItemValue = {
 }
 
 function Index({ invoices, setInvoices }) {
-  const { register, handleSubmit, control, reset, formState: { errors } } = useForm({
+  const { register, watch, handleSubmit, control, reset, formState: { errors } } = useForm({
     defaultValues: {
       items: [defaultItemValue]
     }
@@ -24,6 +24,9 @@ function Index({ invoices, setInvoices }) {
     setInvoices([...invoices, data])
     reset()
   };
+
+  const watchPaymentDate = watch("payment_date");
+  const watchPaymentDueDate = watch("payment_due_date");
 
   return (
     <section onSubmit={handleSubmit(onSubmit)} className="mt-10 max-w-6xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
@@ -73,24 +76,31 @@ function Index({ invoices, setInvoices }) {
             />
             {errors.receiver_email && <span className='text-red-500'>This field is required!</span>}
           </div>
-          <div>
-            <label className="text-gray-700 dark:text-gray-200">Payment Date</label>
-            <input
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
-              type="date"
-              placeholder='Payment Date'
-              {...register("payment_date", { required: false })} />
-          </div>
-          <div>
-            <label className="text-gray-700 dark:text-gray-200">Payment Due Date</label>
-            <input
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
-              type="date"
-              placeholder='Payment Due Date'
-              {...register("payment_due_date", { required: true })}
-            />
-            {errors.payment_due_date && <span className='text-red-500'>This field is required!</span>}
-          </div>
+          {
+            !watchPaymentDueDate && (
+              <div>
+                <label className="text-gray-700 dark:text-gray-200">Payment Date</label>
+                <input
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
+                  type="date"
+                  placeholder='Payment Date'
+                  {...register("payment_date", { required: false })} />
+              </div>
+            )
+          }
+          {
+            !watchPaymentDate && (
+              <div>
+                <label className="text-gray-700 dark:text-gray-200">Payment Due Date</label>
+                <input
+                  className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
+                  type="date"
+                  placeholder='Payment Due Date'
+                  {...register("payment_due_date", { required: false })}
+                />
+              </div>
+            )
+          }
           <div>
             <label className="text-gray-700 dark:text-gray-200">Payment Type</label>
             <select
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -9,7 +9,6 @@ function Index({ invoices }) {
   const [filteredInvoices, setFilteredInvoices] = useState(invoices)
   const [modal, setModal] = useState({ isOpened: false, invoice: null })
 
-
   const filterByInput = (e) => {
     const searchedWords = e.target.value
 
@@ -51,21 +50,28 @@ function Index({ invoices }) {
   };
 
   const onClickEmailButton = (invoice) => {
-    console.log(invoice)
-    toast.success('Email sent successfully!', {
-      position: "top-right",
-      autoClose: 2000,
-      hideProgressBar: false,
-      closeOnClick: true,
-      pauseOnHover: true,
-      draggable: true,
-      progress: undefined,
-    });
+
     emailjs.send('gmail', 'template_invoices', invoice, 'M9wIkM3tvly1rDeUX')
       .then((result) => {
-        console.log(result.text);
+        toast.success('Email sent successfully!', {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
       }, (error) => {
-        console.log(error.text);
+        toast.error('Someting goes wrong!', {
+          position: "top-right",
+          autoClose: 2000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+        });
       });
   }
 
@@ -80,128 +86,125 @@ function Index({ invoices }) {
         </div>
       </div>
       <h2 className="text-3xl font-semibold text-center text-gray-800 dark:text-white font-sans">Fast and easy invoicing</h2>
-      <p className="mt-3 mb-20 text-center text-gray-600 dark:text-gray-400 font-sans">INVOICE online invoice generator makes your life easier when it comes to billing and collecting money.</p>
+      <p className="mt-3 mb-20 text-center text-gray-600 dark:text-gray-400 font-sans">
+        <b className="font-style: italic text-gray-900 font-sans mr-1">INVOICE,</b>
+        online invoice generator makes your life easier when it comes to billing and collecting money.</p>
 
-      <form>
-        <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
-          <input
-            className='mb-10 block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans'
-            placeholder='Filter Invoices'
-            onChange={filterByInput}
-          />
-          <select className='mb-10 block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans'
-            onChange={filterBySelect}>
-            <option value="default">Invoice Payment Status</option>
-            <option value="paid_invocies">Paid Invoice</option>
-            <option value="unpaid_invoices">Unpaid Invoice</option>
-          </select>
-        </div>
-        <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
-            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
-              <tr>
-                <th scope="col" className="py-3 px-6 font-sans">
-                  Title
-                </th>
-                <th scope="col" className="py-3 px-6 font-sans">
-                  Description
-                </th>
-                <th scope="col" className="py-3 px-6 font-sans">
-                  Recipent Name
-                </th>
-                <th scope="col" className="py-3 px-6 font-sans">
-                  Payment Date
-                </th>
-                <th scope="col" className="py-3 px-6 font-sans">
-                  Details
-                </th>
-                <th scope="col" className="py-3 px-6">
-                  <span className="sr-only font-sans">Edit</span>
-                </th>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                filteredInvoices.map((invoice, i) => (
-                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-                    key={i}>
-                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
-                      {invoice.title}
-                    </td>
-                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
-                      {invoice.description}
-                    </td>
-                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
-                      {invoice.recipent}
-                    </td>
-                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
-                      {invoice.payment_date}
-                    </td>
-                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
-                      <button className="px-3 py-1 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600 font-sans" onClick={() => {
-                        setModal({ isOpened: true, invoice: invoice })
-                      }}>Details</button></td>
-                    <td className="py-4 px-6 font-medium font-light text-gray-900 whitespace-nowrap dark:text-white">
-                      <button className="bg-transparent hover:bg-blue-100 text-blue-800 hover:text-white py-1 px-3 border border-blue-500 hover:border-transparent rounded font-sans"
-                        position="top-right"
-                        onClick={() => {
-                          onClickEmailButton(invoice)
-                        }}>Send Email</button></td>
-                  </tr>
-                ))
-              }
-            </tbody>
-          </table>
-          <Modal
-            isOpen={modal.isOpened}
-            style={customStyles}
-            onAfterOpen={afterOpenModal}
-            ariaHideApp={false}
-            onRequestClose={closeModal}
-          >
-            {modal.invoice && (
-              <table className='w-full border mt-10 text-center'>
-                <tbody>
-                  <tr>
-                    <th scope="col" className="py-3 px-6 font-sans">Service</th>
-                    <th scope="col" className="py-3 px-6 font-sans">Quantity</th>
-                    <th scope="col" className="py-3 px-6 font-sans">Price</th>
-                    <th scope="col" className="py-3 px-6 font-sans">Total</th>
-                  </tr>
-                  {
-                    modal.invoice.items.map((item, i) => (
-                      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
-                        key={i}>
-                        <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
-                          {item.service}
-                        </td>
-                        <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
-                          {item.quantity}
-                        </td>
-                        <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
-                          {item.price}
-                        </td>
-                        <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
-                          {item.price * item.quantity}
-                        </td>
-                      </tr>
-                    ))
-                  }
-                </tbody>
-              </table>
-            )}
-          </Modal>
-          <ToastContainer
-            autoClose={2000}
-            hideProgressBar={false}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover />
-        </div>
-      </form>
+      <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
+        <input
+          className='mb-10 block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans'
+          placeholder='Filter Invoices'
+          onChange={filterByInput}
+        />
+        <select className='mb-10 block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans'
+          onChange={filterBySelect}>
+          <option value="default">Invoice Payment Status</option>
+          <option value="paid_invocies">Paid Invoice</option>
+          <option value="unpaid_invoices">Unpaid Invoice</option>
+        </select>
+      </div>
+      <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
+        <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+          <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+              <th scope="col" className="py-3 px-6 font-sans">
+                Title
+              </th>
+              <th scope="col" className="py-3 px-6 font-sans">
+                Recipent Name
+              </th>
+              <th scope="col" className="py-3 px-6 font-sans">
+                Payment Date
+              </th>
+              <th scope="col" className="py-3 px-6 font-sans">
+                Details
+              </th>
+              <th scope="col" className="py-3 px-6">
+                <span className="sr-only font-sans">Edit</span>
+              </th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              filteredInvoices.map((invoice, i) => (
+                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                  key={i}>
+                  <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                    {invoice.title}
+                  </td>
+                  <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                    {invoice.recipent}
+                  </td>
+                  <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                    {invoice.payment_date}
+                  </td>
+                  <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                    <button className="px-3 py-1 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600 font-sans" onClick={() => {
+                      setModal({ isOpened: true, invoice: invoice })
+                    }}>Details</button></td>
+                  <td className="py-4 px-6 font-medium font-light text-gray-900 whitespace-nowrap dark:text-white">
+                    <button className="bg-transparent hover:bg-blue-100 text-blue-800 hover:text-white py-1 px-3 border border-blue-500 hover:border-transparent rounded font-sans"
+                      position="top-right"
+                      onClick={() => {
+                        onClickEmailButton(invoice)
+                      }}>Send Email</button></td>
+                </tr>
+              ))
+            }
+            <Modal
+              isOpen={modal.isOpened}
+              style={customStyles}
+              onAfterOpen={afterOpenModal}
+              ariaHideApp={false}
+              onRequestClose={closeModal}
+            >
+              {modal.invoice && (
+                <div>
+                  <p><b>Description: </b>{ modal.invoice.description }</p>
+                  <table className='w-full border mt-10 text-center'>
+                  <tbody>
+                    <tr>
+                      <th scope="col" className="py-3 px-6 font-sans">Service</th>
+                      <th scope="col" className="py-3 px-6 font-sans">Quantity</th>
+                      <th scope="col" className="py-3 px-6 font-sans">Price</th>
+                      <th scope="col" className="py-3 px-6 font-sans">Total</th>
+                    </tr>
+                    {
+                      modal.invoice.items.map((item, i) => (
+                        <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                          key={i}>
+                          <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                            {item.service}
+                          </td>
+                          <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                            {item.quantity}
+                          </td>
+                          <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                            {item.price}
+                          </td>
+                          <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                            {item.price * item.quantity}
+                          </td>
+                        </tr>
+                      ))
+                    }
+                  </tbody>
+                </table>
+                </div>
+              )}
+            </Modal>
+          </tbody>
+        </table>
+      </div>
+      <ToastContainer
+        autoClose={2000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover />
     </section>
   )
 }

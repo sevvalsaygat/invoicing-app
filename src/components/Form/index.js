8e30ea414a6cda8c431529b610615247ecef0f3d
sevvalsaygat <sevvallsstt@gmail.com>
2022-08-11 15:31:33 +0300

    Add tailwind CSS to list component

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -28,8 +28,8 @@ function Index({ invoices, setInvoices }) {
   return (
     <section onSubmit={handleSubmit(onSubmit)} className="mt-10 max-w-6xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
       <ul className='max-w-7xl mx-auto sm:px-6 text-lg border-b-2 border-gray-300 py-6 md:justify-start md:space-x-10 text-neutral-500 font-Times New Roman' href="#">
-        <span className='ml-10 mr-10 text-3xl font-style: italic text-gray-900'>INVOICE.</span>
-        <Link className='ml-10' to="/list">Fatura Listele</Link>
+        <span className='ml-10 mr-10 text-3xl font-style: italic text-gray-900 font-sans'>INVOICE.</span>
+        <Link className='ml-10 font-sans' to="/list">List Invoice</Link>
       </ul>
       <h2 className="text-lg font-semibold text-gray-700 capitalize dark:text-white">...</h2>
       <form>
@@ -37,7 +37,7 @@ function Index({ invoices, setInvoices }) {
           <div>
             <label className="text-gray-700 dark:text-gray-200">Invoice Title</label>
             <input
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
               type="text"
               placeholder='Invoice Title'
               {...register("title", { required: true })}
@@ -48,7 +48,7 @@ function Index({ invoices, setInvoices }) {
             <label className="text-gray-700 dark:text-gray-200">Description</label>
             <textarea
               rows={5}
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
               placeholder='Description'
               {...register("description")}
             />
@@ -56,7 +56,7 @@ function Index({ invoices, setInvoices }) {
           <div>
             <label className="text-gray-700 dark:text-gray-200">Recipient Name</label>
             <input
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
               type="text"
               placeholder='Recipient Name'
               {...register("recipent", { required: true })}
@@ -66,7 +66,7 @@ function Index({ invoices, setInvoices }) {
           <div>
             <label className="text-gray-700 dark:text-gray-200">Receiver e-mail</label>
             <input
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
               type="email"
               placeholder='Receiver e-mail'
               {...register("receiver_email", { required: true })}
@@ -76,7 +76,7 @@ function Index({ invoices, setInvoices }) {
           <div>
             <label className="text-gray-700 dark:text-gray-200">Payment Type</label>
             <select
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
               {...register("payment_type", { required: true })}>
               <option value="">Please select payment type</option>
               <option value="credit_card">Credit Card</option>
@@ -88,7 +88,7 @@ function Index({ invoices, setInvoices }) {
           <div>
             <label className="text-gray-700 dark:text-gray-200">Payment Date</label>
             <input
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
               type="date"
               placeholder='Payment Date'
               {...register("payment_date", { required: false })} />
@@ -96,7 +96,7 @@ function Index({ invoices, setInvoices }) {
           <div>
             <label className="text-gray-700 dark:text-gray-200">Payment Due Date</label>
             <input
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring"
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
               type="date"
               placeholder='Payment Due Date'
               {...register("payment_due_date", { required: true })}
@@ -112,19 +112,19 @@ function Index({ invoices, setInvoices }) {
                 <table className="min-w-full">
                   <thead className="border-b">
                     <tr>
-                      <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left font-sans">
                         Service
                       </th>
-                      <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left font-sans">
                         Quantity
                       </th>
-                      <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left font-sans">
                         Price
                       </th>
-                      <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left font-sans">
                         Total Price
                       </th>
-                      <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left">
+                      <th scope="col" className="text-sm font-medium text-gray-900 px-6 py-4 text-left font-sans">
                       </th>
                     </tr>
                   </thead>
@@ -132,14 +132,14 @@ function Index({ invoices, setInvoices }) {
                     {
                       fields.map((field, i) => (
                         <tr key={i} className="border-b">
-                          <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                            <input className='mt-5 py-1 px-4 w-30'
+                          <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap font-sans'>
+                            <input className='mt-5 py-1 px-4 w-30 bg-white border border-gray-300 rounded-md'
                               placeholder='Service'
                               {...register(`items[${i}].service`, { required: true })}
                             />
                           </td>
-                          <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                            <input className='mt-5 py-1 px-4 w-16'
+                          <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap font-sans'>
+                            <input className='mt-5 py-1 px-4 w-16 border border-gray-300 rounded-md'
                               placeholder='Quantity'
                               {...register(`items[${i}].quantity`, {
                                 required: true,
@@ -148,8 +148,8 @@ function Index({ invoices, setInvoices }) {
                               })}
                             />
                           </td>
-                          <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                            <input className='mt-5 py-1 px-4 w-16'
+                          <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap font-sans'>
+                            <input className='mt-5 py-1 px-4 w-16 border border-gray-300 rounded-md'
                               placeholder='Price'
                               {...register(`items[${i}].price`, {
                                 required: true,
@@ -160,13 +160,13 @@ function Index({ invoices, setInvoices }) {
                               })}
                             />
                           </td>
-                          <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                            <p className='mt-5 py-1 px-4'>{
+                          <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap font-sans'>
+                            <p className='mt-5 py-1 px-4 w-16 border border-gray-300 rounded-md'>{
                               field.quantity && field.price && field.quantity * field.price
                             }</p>
                           </td>
-                          <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap'>
-                            <button className="ml-10 bg-transparent hover:bg-blue-100 text-blue-800 font-Times New Roman hover:text-white py-1 px-3 border border-blue-500 hover:border-transparent rounded"
+                          <td className='text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap font-sans'>
+                            <button className="mt-5 bg-transparent hover:bg-blue-100 text-blue-800 font-Times New Roman hover:text-white py-1 px-3 border border-blue-500 hover:border-transparent rounded"
                               type='submit'
                               onClick={() => {
                                 remove(i)
@@ -181,13 +181,13 @@ function Index({ invoices, setInvoices }) {
             </div>
           </div>
         </div>
-        <button className="mr-10 mt-10 px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+        <button className="mr-10 mt-10 px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600 font-sans"
           type='submit'
           onClick={() => {
             append(defaultItemValue)
           }}>Add New Item</button>
         <button
-          className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600"
+          className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600 font-sans"
           type='submit'>
           Create
         </button>
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -70,94 +70,140 @@ function Index({ invoices }) {
   }
 
   return (
-    <div>
-      <div className='max-w-7xl mx-auto px-4 sm:px-6'>
+    <section className="mt-10 max-w-6xl p-6 mx-auto bg-white rounded-md shadow-md dark:bg-gray-800">
+      <div className='mb-10 max-w-7xl mx-auto px-4 sm:px-6'>
         <div className='text-lg flex justify-between items-center border-b-2 border-gray-300 py-6 md:justify-start md:space-x-10 text-neutral-500 font-Times New Roman'>
           <ul href="#">
-            <Link className='ml-10' to="/">Yeni fatura oluştur</Link>
+            <span className='ml-10 mr-10 text-3xl font-style: italic text-gray-900 font-sans'>INVOICE.</span>
+            <Link className='ml-10 font-sans' to="/">Create New Invoice</Link>
           </ul>
         </div>
       </div>
-      <div>
+      <h2 className="text-3xl font-semibold text-center text-gray-800 dark:text-white font-sans">Fast and easy invoicing</h2>
+      <p className="mt-3 mb-20 text-center text-gray-600 dark:text-gray-400 font-sans">INVOICE online invoice generator makes your life easier when it comes to billing and collecting money.</p>
 
-        <input
-          className='mt-5 mb-5 mr-10'
-          placeholder='Filter invoices'
-          onChange={filterByInput}
-        />
-        <select onChange={filterBySelect}>
-          <option value="default">Filtreleri Kaldır</option>
-          <option value="paid_invocies">Ödenmiş Faturalar</option>
-          <option value="unpaid_invoices">Ödenmemiş Faturalar</option>
-        </select>
+      <form>
+        <div className='grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2'>
+          <input
+            className='mb-10 block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans'
+            placeholder='Filter Invoices'
+            onChange={filterByInput}
+          />
+          <select className='mb-10 block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans'
+            onChange={filterBySelect}>
+            <option value="default">Invoice Payment Status</option>
+            <option value="paid_invocies">Paid Invoice</option>
+            <option value="unpaid_invoices">Unpaid Invoice</option>
+          </select>
+        </div>
+        <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
 
-        <table className='w-full border mt-10 text-center'>
-          <tbody>
-            <tr>
-              <th>Title</th>
-              <th>Description</th>
-              <th>Recipent Name</th>
-              <th>Payment Date</th>
-              <th>Detaylar</th>
-            </tr>
-            {
-              filteredInvoices.map((invoice, i) => (
-                <tr key={i}>
-                  <td>{invoice.title}</td>
-                  <td>{invoice.description}</td>
-                  <td>{invoice.recipent}</td>
-                  <td>{invoice.payment_date}</td>
-                  <td><button onClick={() => {
-                    setModal({ isOpened: true, invoice: invoice })
-                  }}>Detaylar</button></td>
-                  <td><button position="top-right"
-                    onClick={() => { onClickEmailButton(invoice) }}>Send Email</button></td>
-                </tr>
-              ))
-            }
-          </tbody>
-        </table>
-      </div>
-      <Modal
-        isOpen={modal.isOpened}
-        style={customStyles}
-        onAfterOpen={afterOpenModal}
-        ariaHideApp={false}
-        onRequestClose={closeModal}
-      >
-        {modal.invoice && (
-          <table className='w-full border mt-10 text-center'>
-            <tbody>
+          <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+            <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>
-                <th>Service</th>
-                <th>Quantity</th>
-                <th>Price</th>
-                <th>Total</th>
+                <th scope="col" className="py-3 px-6 font-sans">
+                  Title
+                </th>
+                <th scope="col" className="py-3 px-6 font-sans">
+                  Description
+                </th>
+                <th scope="col" className="py-3 px-6 font-sans">
+                  Recipent Name
+                </th>
+                <th scope="col" className="py-3 px-6 font-sans">
+                  Payment Date
+                </th>
+                <th scope="col" className="py-3 px-6 font-sans">
+                  Details
+                </th>
+                <th scope="col" className="py-3 px-6">
+                  <span className="sr-only font-sans">Edit</span>
+                </th>
               </tr>
+            </thead>
+            <tbody>
               {
-                modal.invoice.items.map((item, i) => (
-                  <tr key={i}>
-                    <td>{item.service}</td>
-                    <td>{item.quantity}</td>
-                    <td>{item.price}</td>
-                    <td>{item.price * item.quantity}</td>
+                filteredInvoices.map((invoice, i) => (
+                  <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                    key={i}>
+                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                      {invoice.title}
+                    </td>
+                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                      {invoice.description}
+                    </td>
+                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                      {invoice.recipent}
+                    </td>
+                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                      {invoice.payment_date}
+                    </td>
+                    <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white">
+                      <button className="px-3 py-1 leading-5 text-white transition-colors duration-200 transform bg-gray-700 rounded-md hover:bg-gray-600 focus:outline-none focus:bg-gray-600 font-sans" onClick={() => {
+                        setModal({ isOpened: true, invoice: invoice })
+                      }}>Details</button></td>
+                    <td className="py-4 px-6 font-medium font-light text-gray-900 whitespace-nowrap dark:text-white">
+                      <button className="bg-transparent hover:bg-blue-100 text-blue-800 hover:text-white py-1 px-3 border border-blue-500 hover:border-transparent rounded font-sans"
+                        position="top-right"
+                        onClick={() => {
+                          onClickEmailButton(invoice)
+                        }}>Send Email</button></td>
                   </tr>
                 ))
               }
             </tbody>
           </table>
-        )}
-      </Modal>
-      <ToastContainer
-        autoClose={2000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover />
-    </div>
+          <Modal
+            isOpen={modal.isOpened}
+            style={customStyles}
+            onAfterOpen={afterOpenModal}
+            ariaHideApp={false}
+            onRequestClose={closeModal}
+          >
+            {modal.invoice && (
+              <table className='w-full border mt-10 text-center'>
+                <tbody>
+                  <tr>
+                    <th scope="col" className="py-3 px-6 font-sans">Service</th>
+                    <th scope="col" className="py-3 px-6 font-sans">Quantity</th>
+                    <th scope="col" className="py-3 px-6 font-sans">Price</th>
+                    <th scope="col" className="py-3 px-6 font-sans">Total</th>
+                  </tr>
+                  {
+                    modal.invoice.items.map((item, i) => (
+                      <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700 hover:bg-gray-50 dark:hover:bg-gray-600"
+                        key={i}>
+                        <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                          {item.service}
+                        </td>
+                        <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                          {item.quantity}
+                        </td>
+                        <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                          {item.price}
+                        </td>
+                        <td className="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white font-sans">
+                          {item.price * item.quantity}
+                        </td>
+                      </tr>
+                    ))
+                  }
+                </tbody>
+              </table>
+            )}
+          </Modal>
+          <ToastContainer
+            autoClose={2000}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover />
+        </div>
+      </form>
+    </section>
   )
 }
 

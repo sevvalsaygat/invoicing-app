c19541c591d46f34ecbacafeaa6e834340bacd3d
sevvalsaygat <sevvallsstt@gmail.com>
2022-08-11 15:38:08 +0300

    Change payment type location on page

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -74,18 +74,6 @@ function Index({ invoices, setInvoices }) {
             {errors.receiver_email && <span className='text-red-500'>This field is required!</span>}
           </div>
           <div>
-            <label className="text-gray-700 dark:text-gray-200">Payment Type</label>
-            <select
-              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
-              {...register("payment_type", { required: true })}>
-              <option value="">Please select payment type</option>
-              <option value="credit_card">Credit Card</option>
-              <option value="check">Check</option>
-              <option value="cash">Cash</option>
-            </select>
-            {errors.payment_type && <span className='text-red-500'>This field is required!</span>}
-          </div>
-          <div>
             <label className="text-gray-700 dark:text-gray-200">Payment Date</label>
             <input
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
@@ -103,7 +91,18 @@ function Index({ invoices, setInvoices }) {
             />
             {errors.payment_due_date && <span className='text-red-500'>This field is required!</span>}
           </div>
-
+          <div>
+            <label className="text-gray-700 dark:text-gray-200">Payment Type</label>
+            <select
+              className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
+              {...register("payment_type", { required: true })}>
+              <option value="">Please select payment type</option>
+              <option value="credit_card">Credit Card</option>
+              <option value="check">Check</option>
+              <option value="cash">Cash</option>
+            </select>
+            {errors.payment_type && <span className='text-red-500'>This field is required!</span>}
+          </div>
         </div>
         <div className="flex flex-col">
           <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -97,7 +97,6 @@ function Index({ invoices }) {
           </select>
         </div>
         <div className="overflow-x-auto relative shadow-md sm:rounded-lg">
-
           <table className="w-full text-sm text-left text-gray-500 dark:text-gray-400">
             <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
               <tr>

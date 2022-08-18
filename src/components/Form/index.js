cf1bb0e46cc9eb5c5eee96d2a350e75ed02c527f
sevvalsaygat <sevvallsstt@gmail.com>
2022-08-18 15:27:29 +0300

    Add regex for email checking

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -82,7 +82,12 @@ function Index() {
               className="block w-full px-4 py-2 mt-2 text-gray-700 bg-white border border-gray-200 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 dark:focus:border-blue-300 focus:outline-none focus:ring font-sans"
               type="email"
               placeholder={t("form.fields.reciver_email")}
-              {...register("receiver_email", { required: true })}
+              {...register("receiver_email", {
+                required: true,
+                pattern: {
+                  value: /[a-zA-Z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,8}(.[a-z{2,8}])?/g
+                },
+              })}
             />
             {errors.receiver_email && <span className='text-red-500'>{t("form.errors.required")}</span>}
           </div>
